881ccdc5b8aa281c6f412af5175fa588332d829c
timzprof <timilehin.olumofin@gmail.com>
2019-10-04 18:47:33 +0100

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import fs from "fs";
+import path from "path";
+import express from "express";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import helmet from "helmet";
+import bcrypt from "bcryptjs";
+import compression from "compression";
+import logger from "morgan";
+import {config} from "dotenv";
+import Sequelize from "sequelize";
+import initializeDatabase from "./util/db";
+import {body} from "express-validator";
+import validator from "./util/validator";
+
+//Models
+
+//Routers
+
+config();
+const URL_PREFIX = "/api/v1";
+const PORT = process.env.PORT || 7000;
+
+// Initialize Database
+const db = initializeDatabase({Sequelize});
+
+// Initialize Models
+
+const app = express();
+
+app.use(helmet());
+app.use(compression());
+app.use(
+	logger("common", {
+		stream: fs.createWriteStream(path.resolve(__dirname, "./logs/access.log"), {
+			flags: "a"
+		})
+	})
+);
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Enable CORS
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, Authorization"
+	);
+	res.header(
+		"Access-Control-Request-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, Authorization"
+	);
+	next();
+});
+
+//Routers 
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	res.status(404).json({
+		error: ["Path does not exist"],
+		status: 404,
+		message: "This route doesn't exist for you!"
+	});
+	next();
+});
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	let responseObj = {
+		status: "error",
+		message: "Something went wrong",
+		errorMessage: error.message
+	};
+	if (process.env.NODE_ENV === "development") {
+		responseObj.errorStack = error.stack;
+	}
+	return res.status(error.statusCode).json(responseObj);
+});
+
+// Connect to Database
+db.sync()
+	.then(() => {
+		console.log("DB Connection has been established");
+		app.listen(PORT);
+		console.log("App Running on PORT", PORT);
+	})
+	.catch(err => {
+		console.error("Failed To connect to Database", err);
+	});
+
+export default app;
